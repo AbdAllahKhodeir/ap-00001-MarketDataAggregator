2f038e280e9c854c95f0fbeb9315ee7d39a4df24
AbdAllah Khodeir <akhodeir3@gatech.edu>
2025-03-03 23:20:25 -0700

    Added test profile

--- src/test/java/com/abdallahkhodeir/trading/MarketDataAggregatorApplicationTests.java
+++ src/test/java/com/abdallahkhodeir/trading/MarketDataAggregatorApplicationTests.java
@@ -1,15 +1,20 @@
 package com.abdallahkhodeir.trading;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class MarketDataAggregatorApplicationTests {
 
+	@Autowired
+	private Environment env;
 	@Test
 	void contextLoads() {
+		System.out.println("✅ Active Profiles: " + String.join(", ", env.getActiveProfiles()));
 	}
 
 }
