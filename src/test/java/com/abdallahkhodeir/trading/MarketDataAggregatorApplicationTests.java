c71abeb1a69902faa4f492c2fdfee6be63e10dff
AbdAllah Khodeir <akhodeir3@gatech.edu>
2025-03-03 22:42:03 -0700

    Configured PostgreSQL & Redis with .env support

---
+++ src/test/java/com/abdallahkhodeir/trading/MarketDataAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdallahkhodeir.trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketDataAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
